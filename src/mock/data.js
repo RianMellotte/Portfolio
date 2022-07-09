c03292a1ea09e8cb3a32b2d3084a78ee1dd4f2a7
Rian Zelda Katja <zelda@Rians-MacBook-Pro.local>
2022-07-09 20:11:39 +0100

    update about me section

--- src/mock/data.js
+++ src/mock/data.js
@@ -18,9 +18,9 @@ export const heroData = {
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpeg',
-  paragraphOne: 'Rian is a Developer based in Edinburgh. Originally from Northern Ireland, he moved to Scotland to study an MSc in Cognitive Science and Philosophy in the University of Edinburgh.',
-  paragraphTwo: 'He has built and delpoyed applications from scratch in various languages, including Javacript, Python, HTML and CSS. ' + 
-                'Most of his spare time is spent either on walks with his wife, Katja and border collie Link, playing chess or listening to obscure podcasts.' ,
+  paragraphOne: 'Rian is a Developer based in Vilanova i la Geltrú, Spain. Originally from Northern Ireland, he fell in love with programming while studying a MSc in Cognitive Science and Philosophy in Edinburgh.',
+  paragraphTwo: 'He has built and delpoyed applications from scratch in various languages, including C#, Typescript, and Python. He currently works as a Full Stack Developer for Edinburgh based start-up, Forrit.' + 
+                'Most of his spare time is spent either on walks with his wife, Katja and border collie Link, playing chess or listening to obscure guitar.' ,
   paragraphThree: 'Rian is open to opportunities to join a small focused team tackling difficult and interesting problems.',
   resume: 'https://rianmellotte.github.io/Resume/', // if no resume, the button will not show up
 };
